5b38dd4b961866b7aa632a0ef7c04809162b0dbc
Charrière Loïc <loic.charriere@students.bfh.ch>
2020-02-13 16:25:30 +0100

    end of part 3

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
+  Route::get('/user', function( Request $request ){
     return $request->user();
+  });
 });
-
-Route::resource('/renders', 'RendersController', [
-    'except' => ['edit', 'show', 'store']
-]);
