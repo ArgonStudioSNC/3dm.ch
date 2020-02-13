4cb7c828e2e059e3f968ecac78d1baab8da6a335
Charrière Loïc <loic.charriere@students.bfh.ch>
2020-02-13 16:45:13 +0100

    end of part 4

--- resources/js/rendersurfer.js
+++ resources/js/rendersurfer.js
@@ -1,12 +1,23 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-require('./bootstrap');
+window._ = require('lodash');
 
-window.Vue = require('vue');
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('foundation-sites');
+} catch (e) {}
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 
 const app = new Vue({
-  el: '#app'
+    el: '#app'
 });
