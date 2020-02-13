f511181eb3b95736b7e3ca8a87e4f0dc404fc9c2
Charrière Loïc <loic.charriere@students.bfh.ch>
2020-02-13 17:09:41 +0100

    end of part 5

--- resources/js/rendersurfer.js
+++ resources/js/rendersurfer.js
@@ -17,7 +17,6 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+import Vue from 'vue';
 
-const app = new Vue({
-    el: '#app'
-});
+new Vue().$mount('#app');
